a8e63301282464d20795af75aaf5546a289b3aa0
cisasmendi <cisasmendi@unca.edu.ar>
2025-02-24 12:59:37 -0300

    add svelte5-router for routing and restructure app components

---
+++ backoffice/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { Router, Route } from "svelte5-router"; // manejo de rutas
+    import Login from "./public/component/Login.svelte";
+    // @ts-ignore
+    import Private from "./private/index.svelte";
+    export let url = "";
+</script>
+
+<svelte:head>
+    <title>Administración</title>
+</svelte:head>
+<Router {url}>
+    <Route path="/" component={Login} />
+    <Route path="/users" component={Private} />
+</Router>
